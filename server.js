67e51527a78e366f9cfc7eb7136b199559c38efa
wilsonckm <wilsonckmchan@gmail.com>
2023-06-22 12:48:30 -0400

    Commit 2: Set up complete for method override, mongoose, and express

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
-const express = require('express');
-const app = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-let PORT = 3000;
-if(process.env.PORT){
-	PORT = process.env.PORT
-}
+require('dotenv').config();
+require('./config/database');
+var methodOverride = require('method-override');
 
-app.get('/', (req, res)=>{
-	res.send('hi');
-})
+var indexRouter = require('./routes/index');
+var exercisesRouter = require('./routes/exercises');
 
-app.listen(PORT, ()=>{
-	console.log('listening');
-})
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.use(methodOverride('_method')); 
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/exercises', exercisesRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
